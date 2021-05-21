3e4db93d9984dbd0d9d32982e44cfc9ea6a681de
Fabio Massaretto <fmassaretto@gmail.com>
2021-05-20 23:28:29 -0300

    added timer and refactored code

--- src/MaxSum.java
+++ src/MaxSum.java
@@ -1,21 +1,41 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class MaxSum {
-  private static int sum = 0;
+  private static List<Integer> getBigList(){
+    List<Integer> bigList = new ArrayList<>();
+    Random random = new Random();
+
+    for (int i = 0; i <= 100000; i++){
+      bigList.add(random.nextInt(500) + 1);
+    }
+
+    return bigList;
+  }
+
   public static int findMaxSum(List<Integer> list) {
-    int max1 = list.stream().max(Comparator.naturalOrder()).get();
-    list.remove(list.stream().max(Comparator.naturalOrder()).get());
-    int max2 = list.stream().max(Comparator.naturalOrder()).get();
+    List<Integer> linkedListRef = new LinkedList<>(list);
+    linkedListRef.sort(Comparator.reverseOrder());
+
+    int max1 = linkedListRef.get(0);
+    linkedListRef.remove(0);
+    int max2 = linkedListRef.get(0);
     return max1 + max2;
   }
 
-  public static void main(String[] args) {
-    List<Integer> listRef = Arrays.asList(5, 9, 7, 11);
-    List<Integer> linkedListRef = new LinkedList<>(listRef);
+  public static int findMaxSum2(List<Integer> list) {
+    list.sort(Comparator.reverseOrder());
 
-    System.out.println(findMaxSum(linkedListRef));
+    return list.stream().limit(2).reduce(0, Integer::sum);
+  }
+
+  public static void main(String[] args) {
+    List<Integer> list = Arrays.asList(5, 9, 7, 11);
+
+    Long start = Calendar.getInstance().getTimeInMillis();
+    System.out.println(findMaxSum(getBigList()));
+//    System.out.println(findMaxSum(list));
+    Long end = Calendar.getInstance().getTimeInMillis();
+
+    System.out.println("Diff: " + (end - start));
   }
 }
